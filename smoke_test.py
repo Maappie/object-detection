c1664cf05e4ff4a0c07fc283bc10a2bdb306a55f
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-08-26 15:37:42 +0800

    delete

--- smoke_test.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO
-m = YOLO("yolov8n.pt")  # auto-downloads tiny model
-res = m("https://ultralytics.com/images/bus.jpg", save=True, verbose=False)
-print("OK. Saved to:", res[0].save_dir)
